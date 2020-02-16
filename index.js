2cf35b5636105d27d6eec7379e7a059ca3eda0a8
Jacob Reilly-Cooper <jacobreilly-cooper@jacobs-mbp.lan>
2020-02-16 21:57:58 +0000

    update with mentalMaths function

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 exports.printMsg = function() {
     console.log("This is a test message from the Code Nation module.");
 }
+
+exports.mentalMaths = function() {
+    let numberOne = 10;
+    let numberTwo = 20;
+    let numberThree = numberOne + numberTwo;
+    console.log(numberThree);
+}
