2311802ba358522ce77d1988953474ed7f6ffd5e
Jacob Reilly-Cooper <jacobreilly-cooper@Jacobs-MacBook-Pro.local>
2020-02-17 09:50:57 +0000

    Update for the NPM package

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ exports.printMsg = function() {
     console.log("This is a test message from the Code Nation module.");
 }
 
-exports.mentalMaths = function() {
-    let numberOne = 10;
-    let numberTwo = 20;
-    let numberThree = numberOne + numberTwo;
-    console.log(numberThree);
+exports.dean = function() {
+    console.log("Hi, Dean.");
 }
