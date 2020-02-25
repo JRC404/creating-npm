4e193fa0d5d18d92fad08f9c5dd33cf9b8850101
Jacob Reilly-Cooper <jacob.reilly.cooper@gmail.com>
2020-02-25 00:37:43 +0000

    harry potter function added. Just for fun.

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-exports.printMsg = function() {
+exports.printMsg = function () {
     console.log("This is a test message from the Code Nation module.");
 }
 
-exports.dean = function() {
+exports.dean = function () {
     console.log("Hi, Dean.");
+}
+
+exports.harryPotter = function () {
+    console.log("The Harry Potter API is located here: https://https://www.potterapi.com/#documentation")
 }
